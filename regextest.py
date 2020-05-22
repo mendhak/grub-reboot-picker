ac7cee78ee654652bae3dd8cf20694773d85ff52
mendhak <mendhak@users.noreply.github.com>
2020-05-22 23:45:15 +0100

    Parse submenu for advanced menu options

--- regextest.py
+++ regextest.py
@@ -1,41 +1,112 @@
 import re
 import json
 
-menu_pattern = re.compile("^menuentry '([^']*)'")
-submenu_pattern = re.compile("^\\s+menuentry '([^']*)'")
+def get_grub_entries():
+    """
+    Builds JSON from grub menu entries, just the top level ones, like this:
+    {
+        "menuitems": [
+            {
+                "name": "Ubuntu"
+            },
+            {
+                "name": "Windows Boot Manager (on /dev/nvme0n1p2)"
+            },
+            {
+                "name": "UEFI Firmware Settings"
+            }
+        ]
+    }
+    """
+    pattern = re.compile("^menuentry '([^']*)'")
+    grub_entries = {}
+    grub_entries.clear()
+    grub_entries['menuitems'] = []
 
-grub_entries = {}
-grub_entries.clear()
-grub_entries['menuitems'] = []
-menu_entry_match = None
-current_submenu = None
-submenu_entry_match = None
+    for i, line in enumerate(open('/boot/grub/grub.cfg')):
+        for match in re.finditer(pattern, line):
+            grub_entry = {}
+            grub_entry['name'] = match.group(1)
+            grub_entries['menuitems'].append(grub_entry)
+            # grub_entries.append(match.group(1))
+    return grub_entries
 
 
-for i, line in enumerate(open('/boot/grub/grub.cfg')):
-    menu_entry_match = re.match(menu_pattern, line)
-    if menu_entry_match:
-        grub_entry = {}
-        grub_entry['name'] = menu_entry_match.group(1)
-        grub_entries['menuitems'].append(grub_entry)
-        #print(menu_entry_match.group(1))
-        current_submenu = grub_entry
-        current_submenu['submenuitems'] = []
-        continue
+def get_grub_entries_with_submenus():
+    """
+    Builds JSON from grub menu entries, with sub menu items like this:
+    {
+        "menuitems": [
+            {
+                "name": "Ubuntu",
+                "submenuitems": [
+                    {
+                        "name": "Ubuntu, with Linux 5.4.0-31-generic"
+                    },
+                    {
+                        "name": "Ubuntu, with Linux 5.4.0-31-generic (recovery mode)"
+                    },
+                    {
+                        "name": "Ubuntu, with Linux 5.4.0-29-generic"
+                    },
+                    {
+                        "name": "Ubuntu, with Linux 5.4.0-29-generic (recovery mode)"
+                    }
+                ]
+            },
+            {
+                "name": "Windows Boot Manager (on /dev/nvme0n1p2)",
+                "submenuitems": []
+            },
+            {
+                "name": "UEFI Firmware Settings",
+                "submenuitems": []
+            }
+        ]
+    }
 
-    if current_submenu:
+    """
+    menu_pattern = re.compile("^menuentry '([^']*)'")
+    submenu_pattern = re.compile("^submenu '([^']*)'")
+    submenu_entry_pattern = re.compile("^\\s+menuentry '([^']*)'")
+
+    grub_entries = {}
+    grub_entries.clear()
+    grub_entries['menuitems'] = []
+    menu_entry_match = None
+    current_submenu = None
+    submenu_entry_match = None
+
+    for i, line in enumerate(open('/boot/grub/grub.cfg')):
+        menu_entry_match = re.match(menu_pattern, line)
+        if menu_entry_match:
+            grub_entry = {}
+            grub_entry['name'] = menu_entry_match.group(1)
+            grub_entries['menuitems'].append(grub_entry)
+            continue
+
         submenu_entry_match = re.match(submenu_pattern, line)
         if submenu_entry_match:
-            #print(submenu_entry_match.group(1))
             grub_entry = {}
             grub_entry['name'] = submenu_entry_match.group(1)
-            current_submenu['submenuitems'].append(grub_entry)
+            grub_entries['menuitems'].append(grub_entry)
+            #print(submenu_entry_match.group(1))
+            current_submenu = grub_entry
+            current_submenu['submenuitems'] = []
+            continue
 
-    #for match in re.finditer(pattern, line):
-    #    grub_entries.append(match.group(1))
+        if current_submenu:
+            submenu_entry_match = re.match(submenu_entry_pattern, line)
+            if submenu_entry_match:
+                #print(submenu_entry_match.group(1))
+                grub_entry = {}
+                grub_entry['name'] = submenu_entry_match.group(1)
+                current_submenu['submenuitems'].append(grub_entry)
+    return grub_entries
 
 
-print(json.dumps(grub_entries, indent=4, sort_keys=True))
+print(json.dumps(get_grub_entries(), indent=4, sort_keys=True))
+print(json.dumps(get_grub_entries_with_submenus(), indent=4, sort_keys=True))
 
 
 
--- tray.py
+++ tray.py
@@ -83,7 +83,8 @@ def get_grub_entries_with_submenus():
 
     """
     menu_pattern = re.compile("^menuentry '([^']*)'")
-    submenu_pattern = re.compile("^\\s+menuentry '([^']*)'")
+    submenu_pattern = re.compile("^submenu '([^']*)'")
+    submenu_entry_pattern = re.compile("^\\s+menuentry '([^']*)'")
 
     grub_entries = {}
     grub_entries.clear()
@@ -98,15 +99,22 @@ def get_grub_entries_with_submenus():
             grub_entry = {}
             grub_entry['name'] = menu_entry_match.group(1)
             grub_entries['menuitems'].append(grub_entry)
-            # print(menu_entry_match.group(1))
+            continue
+
+        submenu_entry_match = re.match(submenu_pattern, line)
+        if submenu_entry_match:
+            grub_entry = {}
+            grub_entry['name'] = submenu_entry_match.group(1)
+            grub_entries['menuitems'].append(grub_entry)
+            #print(submenu_entry_match.group(1))
             current_submenu = grub_entry
             current_submenu['submenuitems'] = []
             continue
 
         if current_submenu:
-            submenu_entry_match = re.match(submenu_pattern, line)
+            submenu_entry_match = re.match(submenu_entry_pattern, line)
             if submenu_entry_match:
-                # print(submenu_entry_match.group(1))
+                #print(submenu_entry_match.group(1))
                 grub_entry = {}
                 grub_entry['name'] = submenu_entry_match.group(1)
                 current_submenu['submenuitems'].append(grub_entry)
